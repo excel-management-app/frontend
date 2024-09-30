f0349fa00720db9d8a96e42cf9a18010739c389e
dreamman <huy.nguyen@myg.vn>
2024-09-30 17:27:48 +0700

    test upload file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
+import React, { useState } from 'react';
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = async (event) => {
+    const file = event.target.files[0];
+    // const reader = new FileReader();
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch("http://localhost:3001/uploadFile", {
+      method: "POST",
+      body: formData,
+    });
+    if (response.ok) {
+      const result = await response.json();
+      // return result.filePath;
+      console.log(result)
+    }
+    // reader.onload = (e) => {
+    //   const text = e.target.result;
+    //   const rows = text.split('\n').map(row => row.split(','));
+    //   setData(rows);
+    // };
+    // reader.readAsText(file);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <input type="file" onChange={handleFileUpload} />
+      <table border="1" style={{ margin: '20px auto', width: '80%' }}>
+        <thead>
+          <tr>
+          </tr>
+        </thead>
+        <tbody>
+        
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
