7dabdea46c4d3394455ae5032e65e9148936eb9e
duyhiep2519 <duyhiep2519@gmail.com>
2024-10-03 11:11:59 +0700

    update get all file

--- src/components/ExcelViewer/hooks/useGetAllFiles.ts
+++ src/components/ExcelViewer/hooks/useGetAllFiles.ts
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query"; // Import React Query's useQuery
 import { FileListOption } from "../../../utils/types";
 import { getAllFiles } from "../../../apis/excel";
 
+const fetchAllFiles = async (): Promise<FileListOption[]> => {
+  return getAllFiles();
+};
+
 export function useGetAllFiles() {
-  const [files, setFiles] = useState<FileListOption[]>([]);
-
-  useEffect(() => {
-    const fetchFiles = async () => {
-      const filesData = await getAllFiles();
-
-      setFiles(filesData);
-    };
-
-    fetchFiles().catch(console.error);
-  }, []);
+  const {
+    data: files = [],
+    isLoading,
+    error,
+    refetch,
+  } = useQuery<FileListOption[]>({
+    queryKey: ["allFiles"],
+    queryFn: fetchAllFiles,
+  });
 
   return {
     files,
+    isLoading,
+    error,
+    refetch,
   };
 }
