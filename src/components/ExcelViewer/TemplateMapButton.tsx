6eb3d25b06a089ceefb14b15a46d8ec858bdd304
dreamman <nvhuy1509@gmail.com>
2024-10-26 22:06:26 +0700

    update for build

--- src/components/ExcelViewer/TemplateMapButton.tsx
+++ src/components/ExcelViewer/TemplateMapButton.tsx
@@ -11,17 +11,7 @@ import { toast } from "react-toastify";
 import { makeStyles } from "tss-react/mui";
 import { useUploadFileMap } from "./hooks/useUploadFile";
 
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+
 
 const useStyles = makeStyles()(() => ({
   button1: {
